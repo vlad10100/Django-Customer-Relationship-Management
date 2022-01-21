3a6d444dd4d8cc800b1ff3be5d46cf485ae52556
vlad10100 <vbucaralde@gmail.com>
2022-01-21 13:37:25 +0800

    CBV

--- leads/urls.py
+++ leads/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path  
 from . import views 
+from .views import LandingPageView, LeadListView, LeadDetailView, LeadCreateView, LeadUpdateView
 
 app_name = 'leads'
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('retrieve/', views.retrieve, name='retrieve'),
-    path('create/', views.create, name='create'),
-    path('detail/<int:pk>', views.detail, name='detail'),
-    path('edit/<int:pk>', views.edit, name='edit'),
+    path('', LandingPageView.as_view(), name='home'),
+    path('retrieve/', LeadListView.as_view(), name='retrieve'),
+    path('detail/<int:pk>', LeadDetailView.as_view(), name='detail'),
+    path('create/', LeadCreateView.as_view(), name='create'),
+    path('edit/<int:pk>', LeadUpdateView.as_view(), name='edit'),
     path('delete/<int:pk>', views.delete, name='delete')
 ]
--- leads/views.py
+++ leads/views.py
@@ -1,64 +1,96 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse
+from django.views.generic import TemplateView, ListView, DetailView, CreateView, UpdateView, DeleteView
+
 from .models import Lead, Agent, User
 from .forms import Lead_Form, Lead_Edit_Form
 
+class LandingPageView(ListView):
+    template_name = 'leads/home.html'
+    queryset = Lead.objects.all()
+    context_object_name = 'leads'
 
-def home(request):
-    lead = Lead.objects.all()
-    context = {'leads':lead}
-    return render(request, 'leads/home.html', context)
+class LeadListView(ListView):
+    template_name = 'leads/retrieve.html'
+    queryset = Lead.objects.all()
+    context_object_name = 'lead'
 
-def retrieve(request):
-    lead = Lead.objects.all()
-    agent = Agent.objects.all() 
-    context = {
-        'lead': lead,
-        'agent': agent
-    }
-    return render(request, 'leads/retrieve.html', context)
+class LeadDetailView(DetailView):
+    template_name = 'leads/detail.html'
+    queryset = Lead.objects.all()
+    context_object_name = 'lead'
+
+class LeadCreateView(CreateView):
+    template_name = 'leads/create.html'
+    form_class = Lead_Form
+    
+    def get_success_url(self):
+        return reverse("leads:home")
+
+class LeadUpdateView(UpdateView):
+    template_name = 'leads/edit.html'
+    form_class = Lead_Edit_Form
+    queryset = Lead.objects.all()
+    
+    def get_success_url(self):
+        return reverse("leads:home")
+
+    
+
+# def home(request):
+#     lead = Lead.objects.all()
+#     context = {'leads':lead}
+#     return render(request, 'leads/home.html', context)
+
+# def retrieve(request):
+#     lead = Lead.objects.all() 
+#     context = {
+#         'lead': lead,
+#     }
+#     return render(request, 'leads/retrieve.html', context)
+
+# def detail(request, pk):
+#     x = Lead.objects.get(id=pk)
+#     print(x)
+#     context = {'lead': x}
+#     return render(request, 'leads/detail.html', context)
 
 
-def create(request):
-    form = Lead_Form()
-    context = {'form':form}
-    if request.method == 'POST':
-        form = Lead_Form(request.POST)
-        if form.is_valid():
-            first_name = form.cleaned_data['first_name']
-            last_name = form.cleaned_data['last_name']
-            age = form.cleaned_data['age']
-            agent = form.cleaned_data['agent']
-            agent = User.objects.get(username=agent)
-            print(agent)
-            agent = form.cleaned_data['agent']
-            Lead.objects.create(first_name=first_name, last_name=last_name,
-                                age=age, agent=agent)
-            print('Successfully created')
-    return render(request, 'leads/create.html', context)
+# def create(request):
+#     form = Lead_Form()
+#     context = {'form':form}
+#     if request.method == 'POST':
+#         form = Lead_Form(request.POST)
+#         if form.is_valid():
+#             first_name = form.cleaned_data['first_name']
+#             last_name = form.cleaned_data['last_name']
+#             age = form.cleaned_data['age']
+#             agent = form.cleaned_data['agent']
+#             agent = User.objects.get(username=agent)
+#             print(agent)
+#             agent = form.cleaned_data['agent']
+#             Lead.objects.create(first_name=first_name, last_name=last_name,
+#                                 age=age, agent=agent)
+#             print('Successfully created')
+#     return render(request, 'leads/create.html', context)
 
-def detail(request, pk):
-    x = Lead.objects.get(id=pk)
-    print(x)
-    context = {'lead': x}
-    return render(request, 'leads/detail.html', context)
 
-def edit(request, pk):
-    lead = Lead.objects.get(id=pk)
-    form = Lead_Edit_Form()
-    context = {'form':form}
-    if request.method == 'POST':
-        form = Lead_Edit_Form(request.POST)
-        if form.is_valid():
-            first_name = form.cleaned_data['first_name']
-            last_name = form.cleaned_data['last_name']
-            age = form.cleaned_data['age']
-            lead.first_name = first_name
-            lead.last_name = last_name
-            lead.age = age 
-            lead.save()
-            return retrieve(request)
+# def edit(request, pk):
+#     lead = Lead.objects.get(id=pk)
+#     form = Lead_Edit_Form()
+#     context = {'form':form}
+#     if request.method == 'POST':
+#         form = Lead_Edit_Form(request.POST)
+#         if form.is_valid():
+#             first_name = form.cleaned_data['first_name']
+#             last_name = form.cleaned_data['last_name']
+#             age = form.cleaned_data['age']
+#             lead.first_name = first_name
+#             lead.last_name = last_name
+#             lead.age = age 
+#             lead.save()
+#             return retrieve(request)
 
-    return render(request, 'leads/edit.html', context)
+#     return render(request, 'leads/edit.html', context)
 
 def delete(request, pk):
     lead = Lead.objects.get(id=pk)
