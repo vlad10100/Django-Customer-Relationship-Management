aaf27aff12a1b00bdae564961feb5f8d6420b5fd
vlad10100 <vbucaralde@gmail.com>
2022-01-23 22:16:28 +0800

    minor changes

--- core/settings.py
+++ core/settings.py
@@ -37,9 +37,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
     'leads',
     'agents',
 ]
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+
+CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
